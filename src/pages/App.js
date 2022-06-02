960cb1b4a738f4203bc0aa8fc9513cb521de3a43
_brln.by <2019110015@student.uigm.ac.id>
2022-06-02 09:18:46 +0700

    remove unused files

--- src/pages/App.js
+++ src/pages/App.js
@@ -2,9 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from "./Home/Home";
 import Location from "./Location/Location";
-import GelLocation from "./Form/GelLocation";
-import CreateGelLocation from "./Form/CreateGelLocation";
-import Cok from "./Form/Cok";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
@@ -16,9 +13,6 @@ function App() {
           <Routes>
             <Route exact path="/" element={<Home/>}/>
             <Route path="/location" element={<Location/>}/>
-            <Route path="/getod" element={<GelLocation/>}/>
-            <Route path="/craeted" element={<CreateGelLocation/>}/>
-            <Route path="/cok" element={<Cok/>}/>
           </Routes>
       </Router>
     </div>
